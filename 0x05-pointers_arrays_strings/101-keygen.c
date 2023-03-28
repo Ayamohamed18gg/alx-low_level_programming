d7da31dbef69c555fa24329960b73014e8cfe474
aya mohamed <aya004707@gmail.com>
2023-03-27 17:36:15 -0700

    a

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,26 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <time.h> 
+#include "main.h" 
  
 /** 
-  * main - generates random passwords for 101-crackme 
-  * Return: zero 
+ * _atoi - converts a string to an integer 
+ * 
+ * @s: string input parameter 
+ * 
+ * Return: converted integer from string 
 */ 
  
-int main(void) 
+int _atoi(char *s) 
 { 
- int sum; 
- char c; 
+ unsigned int num = 0; 
+ int sign = 1; 
  
- srand(time(NULL)); 
- while (sum <= 2645) 
- { 
-  c = rand() % 128; 
-  sum += c; 
-  putchar(c); 
- } 
- putchar(2772 - sum); 
- return (0); 
+ do { 
+  if (*s == '-') 
+   sign *= -1; 
+  else if (*s >= '0' && *s <= '9') 
+   num = (num * 10) + (*s - '0'); 
+  else if (num > 0) 
+   break; 
+ } while (*s++); 
+ 
+ return (num * sign); 
 }
